ae7d28367597d98ebf10aaccbbd936beada80387
Django Mustang <123947685+DjangoMustang@users.noreply.github.com>
2023-05-19 16:57:38 +0530

    Jupyter notebook

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# # In this notebook, Data Science Tools and Ecosystem are summarized with lots of hands-on lab exercises.
+
+# # **Objectives:**
+# - List of popular languages for Data Science
+# - List of popular libraries for Data Science
+# - Examples of arithmetic operations using pyhton
+# - Conversion of minutes to hours using code
+
+# ## Some of the popular languages that Data Scientists use are:
+# ### 1. Python
+# ### 2. R
+# ### 3.  SQL
+# ### 4. Julia
+# ### 5. Scala
+
+# ## Some of the commonly used libraries used by Data Scientists include:
+# ### 1.Scikit-lean
+# ### 2. Pandas
+# ### 3. NumPy
+# ### 4. ggplot
+
+# | Data Science Tools |
+# |-------------------|
+# | R                 |
+# | Python            |
+# | Jupyter Notebook  |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+35 + 34
+
+
+# In[2]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author 
+# Siddhi Shrivastava
+
+# In[ ]:
+
+
+
+
